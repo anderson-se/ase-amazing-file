06c921130e44fc27d9fb020581d034a03fdb107d
anderson-se <anderson.software.engineer@gmail.com>
2023-05-01 15:42:49 -0300

    Create tests project

---
+++ tests/AmazingFile.UnitTests/Domain/LotrConverterTests.cs
@@ -0,0 +1,52 @@
+﻿using AmazingFile.Application.Services;
+using AmazingFile.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AmazingFile.UnitTests.Domain;
+
+public class LotrConverterTests
+{
+    private readonly LotrConverter _lotrConverter;
+
+    public LotrConverterTests()
+    {
+        _lotrConverter = new LotrConverter();
+    }
+
+    [Fact]
+    public async Task Should_ConvertItems()
+    {
+        // Arrange
+        var fileLines = GetFileLines();
+
+        // Act
+        var lines = await _lotrConverter.ConvertLines(fileLines).ToListAsync();
+        var line = (LotrItem)lines.Single(o => o is LotrItem);
+
+        // Assert
+    }
+
+    [Fact]
+    public async Task Should_ConvertHeader()
+    {
+        // Arrange
+        var fileLines = GetFileLines();
+        var provider = Guid.NewGuid().ToString();
+
+        // Act
+        var lines = await _lotrConverter.ConvertLines(fileLines).ToListAsync();
+        var headerLines = lines.Where(o => o is LotrHeader).Cast<LotrHeader>();
+
+        // Assert
+        Assert.Equal(3, headerLines.Count());
+    }
+
+    private static async IAsyncEnumerable<string> GetFileLines()
+    {
+        yield return await Task.FromResult("");
+    }
+}
